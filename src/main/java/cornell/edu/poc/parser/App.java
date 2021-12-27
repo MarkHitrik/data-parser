c5cf37fde000d39ca23e776412bdef313c24a216
Michael Hitrik <michael.hitrik@ellucian.com>
2021-12-26 20:33:44 -0500

    test for mark

--- src/main/java/cornell/edu/poc/parser/App.java
+++ src/main/java/cornell/edu/poc/parser/App.java
@@ -3,6 +3,6 @@ package cornell.edu.poc.parser;
 /** Hello world! */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Mark");
+        System.out.println("Mark-HI");
     }
 }
