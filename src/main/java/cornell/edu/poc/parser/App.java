eb15a27b4285abf8476fdbdfb058db70f94656b6
Mark Hitrik <hitrik.mark@gmail.com>
2021-12-26 20:28:03 -0500

    Test

--- src/main/java/cornell/edu/poc/parser/App.java
+++ src/main/java/cornell/edu/poc/parser/App.java
@@ -1,13 +1,8 @@
 package cornell.edu.poc.parser;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+/** Hello world! */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Mark");
     }
 }
